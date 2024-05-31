99574ea94a720986c0ae51b9e7b5e7a4cbc1eaf0
Filip Navara <filip.navara@gmail.com>
2024-05-31 23:25:47 +0200

    Make the scripts more readable

---
+++ preprocess-openapi-yaml.sh
@@ -0,0 +1,21 @@
+yq eval '
+    # Update OpenAPI version
+    .openapi = "3.0.0" |
+    # Delete `id`, it was renamed to $id
+    del(.components.schemas.[].id) |
+    # Delete annotations
+    del(.. | select(has("annotations")).annotations) |
+    # Update default values to match their type
+    with(.. | select(.default? == "false"); .default = false) |
+    with(.. | select(.default? == "true"); .default = true) |
+    with(.. | select(.default? == "0"); .default = 0) |
+    # Delete non-standard enumDescriptions and enumDeprecated
+    del(.. | select(has("enumDescriptions")).enumDescriptions) |
+    del(.. | select(has("enumDeprecated")).enumDeprecated) |
+    # Update int64 to integer
+    with(.. | select(.format? == "int64"); .type = "integer") |
+    # Add error responses
+    .paths.[].[]?.["responses"]? += {"4XX":{"description":"Failure response","content":{"application/json":{"schema":{"$ref":"#/components/schemas/GoogleApiException"}}}}} |
+    .paths.[].[]?.["responses"]? += {"5XX":{"description":"Failure response","content":{"application/json":{"schema":{"$ref":"#/components/schemas/GoogleApiException"}}}}} |
+    . * load("error.yaml")
+    ' $1
