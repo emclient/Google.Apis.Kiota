b31a3e27327820ba3b912f152cef8cfa1c5d0684
Filip Navara <filip.navara@gmail.com>
2024-05-31 23:59:25 +0200

    Switch to our fork of google-discovery-to-openapi

--- generate-openapi.sh
+++ generate-openapi.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/stackql/google-discovery-to-openapi.git
+git clone https://github.com/emclient/google-discovery-to-openapi.git
 pushd google-discovery-to-openapi
 npm install
 chmod +x ./bin/google-discovery-to-openapi.mjs
