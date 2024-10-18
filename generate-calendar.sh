876fb144df273a6ff218e98a605a87e02479e56b
Filip Navara <filip.navara@gmail.com>
2024-10-18 09:20:38 +0200

    Attempt to fix eventTypes scheme

--- generate-calendar.sh
+++ generate-calendar.sh
@@ -1,7 +1,9 @@
 ./preprocess-openapi-yaml.sh google-discovery-to-openapi/openapi/googleapis.com/v00.00.00000/services/calendar/calendar.yaml > src/Google.Apis.Kiota.Calendar/calendar.generated.yaml
 yq eval --inplace '
     # Change any to object
-    .components.schemas.EventWorkingLocationProperties.properties.homeOffice.type = "object"
+    .components.schemas.EventWorkingLocationProperties.properties.homeOffice.type = "object" |
+    # Fix eventTypes to array
+    (.paths.["/calendars/{calendarId}/events"].get.parameters.[] | select(.name == "eventTypes")) += {"schema":{"type":"array","items":{"type":"string"}},"explode":"true"}
     ' src/Google.Apis.Kiota.Calendar/calendar.generated.yaml
 kiota generate --openapi src/Google.Apis.Kiota.Calendar/calendar.generated.yaml -o src/Google.Apis.Kiota.Calendar/Generated -l CSharp -n Google.Apis.Kiota.Calendar -c CalendarApiClient -s Microsoft.Kiota.Serialization.Json.JsonSerializationWriterFactory --ds Microsoft.Kiota.Serialization.Json.JsonParseNodeFactory
 rm src/Google.Apis.Kiota.Calendar/Generated/Models/GoogleApiException.cs
