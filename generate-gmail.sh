65a53af4c11793f8673006db33dc9a97401e218f
Peter Gasparik <gasparik@emclient.com>
2025-04-04 15:59:01 +0200

    Revert "Change int to string for vacationSettings startTime and endTime as the API works with strings"
    This reverts commit 1029337af82f1950fa5d043266826519beba007c.

--- generate-gmail.sh
+++ generate-gmail.sh
@@ -1,9 +1,4 @@
 ./preprocess-openapi-yaml.sh google-discovery-to-openapi/openapi/googleapis.com/v00.00.00000/services/gmail/gmail.yaml > src/Google.Apis.Kiota.Gmail/gmail.generated.yaml
-yq eval --inplace '
-    # Change VacationSettings startTime and endTime to string
-    .components.schemas.VacationSettings.properties.startTime.type = "string" |
-    .components.schemas.VacationSettings.properties.endTime.type = "string"
-    ' src/Google.Apis.Kiota.Gmail/gmail.generated.yaml
 kiota generate --openapi src/Google.Apis.Kiota.Gmail/gmail.generated.yaml -o src/Google.Apis.Kiota.Gmail/Generated -l CSharp -n Google.Apis.Kiota.Gmail -c GmailApiClient -s Microsoft.Kiota.Serialization.Json.JsonSerializationWriterFactory --ds Microsoft.Kiota.Serialization.Json.JsonParseNodeFactory
 rm src/Google.Apis.Kiota.Gmail/Generated/Models/GoogleApiException.cs
 rm src/Google.Apis.Kiota.Gmail/Generated/Models/RequestError.cs
